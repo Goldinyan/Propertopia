90be1807fd28b40ab7eca1eae48f71285d3d168a
Ansgar Seifert <seifert.ansgar@gmail.com>
2025-11-28 12:44:04 +0100

    Update Player.java (School)

--- propertopia-backend/src/main/java/com/propertopia/model/Player.java
+++ propertopia-backend/src/main/java/com/propertopia/model/Player.java
@@ -1,9 +1,11 @@
 package com.propertopia.model;
+import java.util.list;
 
 public class Player {
     private String name;
     private int money;
     private int position;
+    private List<Field> properties;
 
     public Player(String name) {
         this.name = name;
@@ -14,6 +16,12 @@ public class Player {
     public String getName() { return name; }
     public int getMoney() { return money; }
     public int getPosition() { return position; }
+    public List<Field> getProperties() {
+        if(properties !== null){ 
+            return properties;
+        }
+        return null;
+    }
 
     public void move(int steps) {
         position = (position + steps) % 40; 
